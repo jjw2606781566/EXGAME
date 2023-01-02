e2f9dd0c163dbef32ea1a218bf27ba8f62aea8a5
jjw2606781566 <87257275+jjw2606781566@users.noreply.github.com>
2023-01-02 20:46:50 +0800

    Comment类完成

--- RegisterLogin/BusinessClass/Comment.cs
+++ RegisterLogin/BusinessClass/Comment.cs
@@ -2,6 +2,8 @@
 using System;
 using DataBase;
 using System.Collections.Generic;
+using System.Drawing;
+using System.ComponentModel.Design;
 
 
 namespace BusinessClass.Comment
@@ -89,6 +91,257 @@ namespace BusinessClass.Comment
                 }
             }
             return true;
+        }
+        public static bool DeleteComment(string user_id, string game_id, out string reason)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"DELETE FROM COMMENTS WHERE USER_ID='{user_id}' AND GAME_ID='{game_id}'";
+            try
+            {
+                int rslt = cmd.ExecuteNonQuery();
+                cmd.CommandText = rslt == 1 ? "COMMIT" : "ROLLBACK";
+                if (rslt == 0)
+                {
+                    reason = "删除失败";
+                    return false;
+                }
+                else
+                {
+                    reason = "删除成功";
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                cmd.CommandText = "ROLLBACK";
+                reason = e.Message.ToString();
+                return false;
+            }
+            cmd.ExecuteNonQuery();
+            return true;
+        }
+        public static bool EvaluateComment(string user_id, string game_id, int evaluation, out string reason)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = "select LIKE_CMT from COMMENT_LIKES WHERE USER_ID = '" + user_id + ";";
+            OracleDataReader reader = cmd.ExecuteReader();
+            int like_cmt = 0;
+            if (!reader.HasRows)
+            {
+                reason = "用户并未评论过该帖子";
+                return false;       
+            }
+            else
+            {
+                try
+                {
+                    //查找成功
+                    if (reader.Read())
+                    {
+                        like_cmt = System.Convert.ToInt32(reader[0].ToString());
+                    }
+                }
+                catch (Exception e)
+                {
+                    reason = e.Message.ToString();
+                    return false;
+                }
+            }
+            string newview = "";
+            if (evaluation == 1)
+                newview = "AGREE_NUM";
+            else if (evaluation == -1)
+                newview = "DISAGREE_NUM";
+            int cen;
+            if (like_cmt == evaluation)//若赞/踩了还赞/踩，则是取消
+            {
+                cmd.CommandText = "DELETE FROM COMMENT_LIKES WHERE USER_ID = '" + user_id + ";";
+                try
+                {
+                    cen = cmd.ExecuteNonQuery();
+                    if (cen == 0)
+                    {
+                        cmd.CommandText = "ROLLBACK";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "取消操作失败1!";
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cen = cmd.ExecuteNonQuery();
+                    reason = e.Message;
+                    return false;
+                }
+                cmd.CommandText = "UPDATE COMMENTS SET " + newview + "= " + newview + "-1 ;";
+                try
+                {
+                    cen = cmd.ExecuteNonQuery();
+                    if (cen == 0)
+                    {
+                        cmd.CommandText = "ROLLBACK";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "取消操作失败2!";
+                        return false;
+                    }
+                    else//取消操作成功
+                    {
+                        cmd.CommandText = "COMMIT";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "取消操作成功！";
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cen = cmd.ExecuteNonQuery();
+                    reason = e.Message;
+                    return false;
+                }
+                return true;
+            }
+            else if (like_cmt == 0)//没有点过赞/踩，则新增数据
+            {
+                cmd.CommandText = "insert into COMMENT_LIKES values('" + user_id + "," + evaluation + ")";
+                try
+                {
+                    cen = cmd.ExecuteNonQuery();
+                    if (cen == 0)
+                    {
+                        cmd.CommandText = "ROLLBACK";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "新增操作失败1!";
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cen = cmd.ExecuteNonQuery();
+                    reason = e.Message;
+                    return false;
+                }
+                cmd.CommandText = "UPDATE COMMENTS SET " + newview + "= " + newview + "+1";
+                try
+                {
+                    cen = cmd.ExecuteNonQuery();
+                    if (cen == 0)
+                    {
+                        cmd.CommandText = "ROLLBACK";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "新增操作失败2!";
+                        return false;
+                    }
+                    else//取消操作成功
+                    {
+                        cmd.CommandText = "COMMIT";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "新增操作成功！";
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cen = cmd.ExecuteNonQuery();
+                    reason = e.Message;
+                    return false;
+                }
+                return true;
+            }
+            else//点过踩/赞，但此时点了另一个，则取消上一个，新增下一个
+            {
+                string lastview = "";
+                if (like_cmt == -1)
+                    lastview = "DISAGREE_NUM";
+                else
+                    lastview = "AGREE_NUM";
+                cmd.CommandText = "DELETE FROM COMMENT_LIKES WHERE USER_ID = '" + user_id + "'";
+                try
+                {
+                    cen = cmd.ExecuteNonQuery();
+                    if (cen == 0)
+                    {
+                        cmd.CommandText = "ROLLBACK";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "修改操作失败1!";
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cen = cmd.ExecuteNonQuery();
+                    reason = e.Message;
+                    return false;
+                }
+                cmd.CommandText = "insert into COMMENT_LIKES values('" + user_id + "'," + evaluation + ")";
+                try
+                {
+                    cen = cmd.ExecuteNonQuery();
+                    if (cen == 0)
+                    {
+                        cmd.CommandText = "ROLLBACK";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "修改操作失败2!";
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cen = cmd.ExecuteNonQuery();
+                    reason = e.Message;
+                    return false;
+                }
+                cmd.CommandText = "UPDATE COMMENTS SET " + lastview + "= " + lastview + "-1";
+                try
+                {
+                    cen = cmd.ExecuteNonQuery();
+                    if (cen == 0)
+                    {
+                        cmd.CommandText = "ROLLBACK";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "修改操作失败3!";
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cen = cmd.ExecuteNonQuery();
+                    reason = e.Message;
+                    return false;
+                }
+                cmd.CommandText = "UPDATE COMMENTS SET " + newview + "= " + newview + "+1";
+                try
+                {
+                    cen = cmd.ExecuteNonQuery();
+                    if (cen == 0)
+                    {
+                        cmd.CommandText = "ROLLBACK";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "修改操作失败4!";
+                        return false;
+                    }
+                    else//取消操作成功
+                    {
+                        cmd.CommandText = "COMMIT";
+                        cen = cmd.ExecuteNonQuery();
+                        reason = "修改操作成功！";
+                    }
+                }
+                catch (Exception e)
+                {
+                    cmd.CommandText = "ROLLBACK";
+                    cen = cmd.ExecuteNonQuery();
+                    reason = e.Message;
+                    return false;
+                }
+                return true;//防止编译错误
+            }
         }
     }
 }
--- RegisterLogin/Controllers/HomeController.cs
+++ RegisterLogin/Controllers/HomeController.cs
@@ -11,13 +11,9 @@ namespace Controllers.GameInfoController
     [ApiController]
     public class GameInfoController : Controller
     {
-        public class GetCommentRequest
-        {
-            public string game_id { get; set; }
-        }
         [HttpPost]
         [Route("api/GameInfoSubsys/GetComment")]
-        public Dictionary<string, dynamic> GetCommentController([FromBody] GetCommentRequest req)
+        public Dictionary<string, dynamic> GetCommentController([FromBody] Comment req)
         {
             Dictionary<string, dynamic> resp = new Dictionary<string, dynamic>();
             int page = 0;
@@ -37,6 +33,34 @@ namespace Controllers.GameInfoController
             Dictionary<string, dynamic> resp = new Dictionary<string, dynamic>();
             string reason;
             bool result = Comment.PublishComment(req, out reason);
+            resp.Add("result", result);
+            resp.Add("reason", reason);
+            return resp;
+        }
+        [HttpPost]
+        [Route("api/GameInfoSubsys/DeleteComment")]
+        public Dictionary<string, dynamic> DeleteCommentController([FromBody] Comment req)
+        {
+            Dictionary<string, dynamic> resp = new Dictionary<string, dynamic>();
+            string reason;
+            bool result = Comment.DeleteComment(req.user_id, req.game_id, out reason);
+            resp.Add("result", result);
+            resp.Add("reason", reason);
+            return resp;
+        }
+        public class EvaluateCommentRequest
+        {
+            public string user_id;
+            public string game_id;
+            public int evaluation;
+        }
+        [HttpPost]
+        [Route("api/GameInfoSubsys/EvaluateComment")]
+        public Dictionary<string, dynamic> EvaluateCommentController([FromBody] EvaluateCommentRequest req)
+        {
+            Dictionary<string, dynamic> resp = new Dictionary<string, dynamic>();
+            string reason;
+            bool result = Comment.EvaluateComment(req.user_id, req.game_id, req.evaluation, out reason);
             resp.Add("result", result);
             resp.Add("reason", reason);
             return resp;
