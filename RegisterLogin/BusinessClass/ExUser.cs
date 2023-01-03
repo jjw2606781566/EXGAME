d2bbf79f4eb7045d112a5bb6857d9c5428603e4f
陈亮奇 <91824844+LuWa-Chen@users.noreply.github.com>
2023-01-03 19:14:17 +0800

    Add files via upload

---
+++ RegisterLogin/BusinessClass/ExUser.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace exgame.BusinessClass
+{
+    public class User
+    {
+        public string id;
+        public string name;
+        public string email;
+        public string password;
+        public string area;
+        public string register_time;
+        public string last_login_time;
+
+        public static int Login(string email, string password, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT PASSWORD FROM GAME_USER WHERE EMAIL = '{email}'";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                if (reader.Read())
+                {
+                    if(reader[0].ToString() == password)
+                    {
+                        
+                        ret = 0;
+                        result = "";
+                    }
+                    else
+                    {
+                        ret = 1;
+                        result = "";
+                    }
+                }
+                else
+                {
+                    ret = 2;
+                    result = "";
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+            }
+            return ret;
+        }
+        public static int UpdatePassword(string uid, string password_old, string password, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT PASSWORD FROM GAME_USER WHERE ID = '{uid}'";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                if (reader.Read())
+                {
+                    if (reader[0].ToString() == password_old)
+                    {
+                        cmd.CommandText = $"UPDATE GAME_USER SET PASSWORD = '{password}' WHERE ID = '{uid}'";
+                        ret = 1;
+                        result = "";
+                        StateOn(uid,"");
+                    }
+                    else
+                    {
+                        ret = 0;
+                        result = "";
+                    }
+                }
+                else
+                {
+                    ret = -1;
+                    result = "用户不存在";
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+            }
+
+            return ret;
+        }
+        public static int StateOn(string uid, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT STATUS FROM GAME_USER WHERE ID = '{uid}'";
+            
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                if (reader.Read())
+                {
+                    cmd.CommandText = $"UPDATE GAME_USER SET STATUS = '1' WHERE ID = '{uid}'";
+                    ret = 0;
+                    result = "";
+                }
+                else
+                {
+                    ret = -1;
+                    result = "用户不存在";
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+            }
+
+            return ret;
+        }
+        public static int StateOff(string uid, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT STATUS FROM GAME_USER WHERE ID = '{uid}'";
+
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                if (reader.Read())
+                {
+                    cmd.CommandText = $"UPDATE GAME_USER SET STATUS = '0' WHERE ID = '{uid}'";
+                    ret = 0;
+                    result = "";
+                }
+                else
+                {
+                    ret = -1;
+                    result = "用户不存在";
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+            }
+
+            return ret;
+        }
+    }
+}
--- RegisterLogin/BusinessClass/Player.cs
+++ RegisterLogin/BusinessClass/Player.cs
@@ -6,10 +6,8 @@ using Oracle.ManagedDataAccess.Client;
 
 namespace exgame.BusinessClass
 {
-    public class Player
+    public class Player:User
     {
-        public string id;
-        public string name;
         public DateTime birthday;
         public string intro;
         public string profile_photo;
@@ -42,6 +40,219 @@ namespace exgame.BusinessClass
                 result = e.Message;
             }
 
+            return ret;
+        }
+        public static int Register(string email, string name, string password, string time, string area, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT ID FROM GAME_USER WHERE EMAIL = '{email}'";
+            OracleDataReader reader1 = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                if (reader1.Read())
+                {
+                    ret = 2;
+                    result = "";
+                    return ret;
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+                return ret;
+            }
+
+            cmd.CommandText = $"SELECT ID FROM GAME_USER WHERE NAME = '{name}'";
+            OracleDataReader reader2 = cmd.ExecuteReader();
+            try
+            {
+                if (reader2.Read())
+                {
+                    ret = 1;
+                    result = "";
+                    return ret;
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+                return ret;
+            }
+
+            OracleCommand command = DBHelper.con.CreateCommand();
+            command.CommandText = $"SELECT ID FROM GAME_USER WHERE NAME=BASIC_USER";
+            string uid = "";
+            OracleDataReader reader;
+
+            try
+            {
+                reader = command.ExecuteReader();
+                if (reader.Read())
+                    uid = reader[0].ToString();
+                /*int rslt = SetBasicUID(uid);
+                if (rslt == 0)
+                {
+                    command.CommandText = "ROLLBACK";       //update failed 
+                    uid = "";
+                }
+                else
+                    command.CommandText = "COMMIT";*/
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                uid = "";
+            }
+            int res = 0;
+            string new_id = (int.Parse(uid) + 1).ToString("d10");
+
+            //OracleCommand cmd2 = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"UPDATE GAME_USER SET ID='{new_id}' WHERE NAME=BASIC_USER";
+
+            try
+            {
+                res = cmd.ExecuteNonQuery();
+                cmd.CommandText = res == 0 ? "ROLLBACK" : "COMMIT";
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                cmd.CommandText = "ROLLBACK";
+                cmd.ExecuteNonQuery();
+            }
+
+            cmd.CommandText = $"INSERT INTO GAME_USER VALUES('{uid}', '{password}', '{name}', 0, 0, '{email}', null, null, null, '{area}', '{time}')";
+            ret = 0;
+            return ret;
+        }
+        public static int getPlayerInfo(string uid, string name, string email, string password, string area, int status, string birthday, int game_num, string intro, string profile_photo, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT NAME, EMAIL, PASSWORD, AREA, STATUS, BIRTHDAY, GAME_NUM, PROFILE_PHOTO, INTRO FROM GAME_USER WHERE ID = '{uid}'";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                name = reader[0].ToString();
+                email = reader[1].ToString();
+                password = reader[2].ToString();
+                area = reader[3].ToString();
+                status = int.Parse(reader[4].ToString());
+                birthday = reader[5].ToString();
+                game_num = int.Parse(reader[6].ToString());
+                profile_photo = reader[7].ToString();
+                intro = reader[8].ToString();
+                result = "";
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+            }
+            return ret;
+        }
+        public static int UpdatePlayerInfo(string uid, string name, string password, string area, string birthday, string intro, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT NAME FROM GAME_USER WHERE ID = '{uid}'";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                if (reader.Read())
+                {
+                    cmd.CommandText = $"UPDATE GAME_USER SET NAME='{name}', PASSWORD='{password}', AREA='{area}', BIRTHDAY='{birthday}', INTRO='{intro}', WHERE ID='{uid}'";
+                    result = "";
+                }
+                else
+                {
+                    ret = -1;
+                    result = "用户不存在";
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+            }
+            return ret;
+        }
+        public static int UpdateEmail(string uid, string email,string password, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT ID FROM GAME_USER WHERE EMAIL = '{email}'";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                if (!reader.Read())
+                {
+                    cmd.CommandText = $"SELECT PASSWORD FROM GAME_USER WHERE ID = '{uid}'";
+                    reader = cmd.ExecuteReader();
+                    if (reader[0].ToString() == password)
+                    {
+                        cmd.CommandText = $"UPDATE GAME_USER SET EMAIL = '{email}' WHERE ID = '{uid}'";
+                        ret = 0;
+                        result = "";
+                    }
+                    else
+                    {
+                        ret = 1;
+                        result = "";
+                    }
+                }
+                else
+                {
+                    ret = 2;
+                    result = "";
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+            }
+            return ret;
+        }
+        public static int UpdateAvatar(string uid, string avatar, string result)
+        {
+            DBHelper.isOpened();
+            OracleCommand cmd = DBHelper.con.CreateCommand();
+            cmd.CommandText = $"SELECT NAME FROM GAME_USER WHERE ID = '{uid}'";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            int ret = 0;
+            try
+            {
+                if (reader.Read())
+                {
+                    cmd.CommandText = $"UPDATE GAME_USER SET PROFILE_PHOTO = '{avatar}' WHERE ID = '{uid}'";
+                    result = "";
+                }
+                else
+                {
+                    ret = -1;
+                    result = "用户不存在";
+                }
+            }
+            catch (Exception e)
+            {
+                ret = -1;
+                result = e.Message;
+            }
             return ret;
         }
     }
